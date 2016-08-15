92dfce3f792cf0944006583fc42c52999cb2caf7
Chungyuen Li <lizoyu1@gmail.com>
2016-08-15 10:42:53 +0800

    final commit of project No.5 in Python

--- No.5_gradientOptimize.py
+++ No.5_gradientOptimize.py
@@ -9,11 +9,12 @@ while (err > marginErr):
     f_old = f
     fx = 2 * x - 6
     fy = 2 * y - 4
-    coe_g = np.array(([np.power(fx, 2) + np.power(fy, 2), 2*x*fx + 6*fx + 2*y*fy + 4*fy]))
-    diff_g = np.array(([2*coe_g[0], coe_g[1]]))
+    coe_g = np.array(([np.power(fx, 2) + np.power(fy, 2), 2*(x-3)*fx + 2*(y-2)*fy]))
+    diff_g = np.array(([2*coe_g[0], coe_g[1]]), dtype = np.float)
     h_solved = diff_g[1] / diff_g[0]
     x = x - fx*h_solved
     y = y - fy*h_solved
+    f =  np.power(x-3, 2) + np.power(y-2, 2)
     if (f == 0):
         err = 0
     else:
